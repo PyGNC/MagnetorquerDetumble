2a26603919c8ef4008101c3cf31bfd8fc3616114
Jacob Willis <jbwillis272@gmail.com>
2023-04-11 13:59:03 -0400

    make everything static if possible

--- magnetorquer_detumble/bcross.py
+++ magnetorquer_detumble/bcross.py
@@ -20,8 +20,15 @@ class Controller:
                  inclination,
                  minimum_inertia_moment,
                  maximum_dipoles,
-                 output_range):
-        self.k_gain = self._bcross_gain(semi_major_axis, inclination, minimum_inertia_moment)
+                 output_range,
+                 k_gain=None):
+
+        if k_gain is None:
+            # default to optimal gain
+            self.k_gain = self._bcross_gain(semi_major_axis, inclination, minimum_inertia_moment)
+        else:
+            self.k_gain = k_gain
+
         self.maximum_dipoles = np.array(maximum_dipoles)
         self.output_range = np.array(output_range)
 
@@ -49,7 +56,8 @@ class Controller:
         k_gain = 2 * Omega * (1 + np.sin(xi_m)) * Jmin  # equation 30
         return k_gain
 
-    def _bcross_control(self, angular_rate, magnetic_vector_body, k_gain=None):
+    @staticmethod
+    def _bcross_control(angular_rate, magnetic_vector_body, k_gain):
         """ bcross_control(x, epc)
         Detumble controller
         See Markley and Crassidis eq 7.48, p 308
@@ -60,34 +68,34 @@ class Controller:
 
         :return control_dipole: the dipole to produce, units in Am^2
         """
-        if k_gain is None:
-            k_gain = self.k_gain
         omega = angular_rate
         B = magnetic_vector_body
-        Bnorm = np.norm(B)
+        Bnorm = np.linalg.norm(B)
         b = B / Bnorm
         m = (k_gain / Bnorm) * np.cross(omega, b)
         control_dipole = m
         return control_dipole
 
-    def _saturate_dipole(self, control_dipole):
+    @staticmethod
+    def _saturate_dipole(control_dipole, maximum_dipoles):
         """
         Clip the minimum and maximum values of `control_dipole` to be in the range
         [-self.maximum_dipoles, self.maximum_dipoles].
 
         :param control_dipole: the raw control dipole computed by _bcross_control
         """
-        return np.clip(control_dipole, -self.maximum_dipoles, self.maximum_dipoles)
+        return np.clip(control_dipole, -maximum_dipoles, maximum_dipoles)
 
-    def _scale_dipole(self, saturated_control_dipole):
+    @staticmethod
+    def _scale_dipole(saturated_control_dipole, maximum_dipoles, output_range):
         """
         Rescale `saturated_control_dipole` to be in the range set by self.output_range.
         The input should be saturated with `_saturate_dipole` prior to calling this function.
         The return value will be in the range [-self.output_range, self.output_range]
         """
-        return (saturated_control_dipole / self.maximum_dipoles) * self.output_range
+        return (saturated_control_dipole / maximum_dipoles) * output_range
 
-    def get_control(self, angular_rate_body, magnetic_vector_body, k_gain=None):
+    def get_control(self, angular_rate_body, magnetic_vector_body):
         """
         Get the control command for producing a magnetic dipole that will detumble the satellite.
 
@@ -97,7 +105,7 @@ class Controller:
 
         :return scaled_control_dipole: the computed control dipole, in the body frame, scaled to [-output_range, output_range]
         """
-        control_dipole = self._bcross_control(angular_rate_body, magnetic_vector_body, k_gain=k_gain)
-        saturated_control_dipole = self._saturate_dipole(control_dipole)
-        scaled_control_dipole = self._scale_dipole(saturated_control_dipole)
+        control_dipole = self._bcross_control(angular_rate_body, magnetic_vector_body, self.k_gain)
+        saturated_control_dipole = self._saturate_dipole(control_dipole, self.maximum_dipoles)
+        scaled_control_dipole = self._scale_dipole(saturated_control_dipole, self.maximum_dipoles, self.output_range)
         return scaled_control_dipole
